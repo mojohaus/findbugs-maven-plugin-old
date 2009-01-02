2f19aae17600d421788ff45d63109d176e63f801
Garvin LeClaire <garvin.leclaire@gmail.com>
2009-01-02 00:47:28 +0000

    Add test script to test plugin with different versions of Maven
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/findbugs-maven-plugin@8514 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ versionTest.groovy
@@ -0,0 +1,58 @@
+#!/usr/bin/env groovy
+
+def javaHome = System.getenv("JAVA_HOME")
+File logFile
+
+def cli = new CliBuilder(usage:'versionTest -m maven.top.directory -p path  <-l logfile>')
+cli.h(longOpt: 'help', 'usage information')
+cli.m(argName: 'maven.top.directory',  longOpt: 'maven', required: true, args: 1, type:GString, 'Maven top directory containing version of Mven for test')
+cli.p(argName: 'path',  longOpt: 'path', required: true, args: 1, type:GString, 'Path for System')
+cli.l(argName: 'log',  longOpt: 'logFile', required: false, args: 1, type:GString, 'Optional log file')
+
+
+def opt = cli.parse(args)
+if (!opt) { return }
+if (opt.h) opt.usage()
+if (opt.m) mavenTopDir = opt.m
+if (opt.p) executePath = opt.p
+if (opt.l) logFile = new File(opt.l)
+
+println "Maven Top Directory is ${mavenTopDir}"
+
+mavenDirs = []
+
+new File(mavenTopDir).eachDir{ dir ->
+  if (dir.name.startsWith("maven-")) {
+    println "This Maven directory is ${dir.name}"
+    mavenDirs << dir
+  }
+}
+
+println mavenDirs
+
+String[] ENVtoArray() { ENV.collect { k, v -> "$k=$v" } }
+
+mavenDirs.each(){ mavenDir ->
+  ENV = [JAVA_HOME: javaHome]
+  ENV.M2_HOME = mavenDir.getAbsolutePath()
+  ENV.PATH = executePath + File.pathSeparator + "${mavenDir}/bin"
+  ENVtoArray()
+  
+  proc = "mvn -version".execute(ENVtoArray(), null)
+  proc.waitFor()
+  procText = proc.text
+  println procText
+
+  if (logFile) {
+    logFile << procText
+  }
+
+  proc = "${mavenDir}/bin/mvn -Dshit=true install".execute(ENVtoArray(), null)
+  proc.waitFor()
+  procText = proc.text
+  println procText
+
+  if (logFile) {
+    logFile << procText
+  }
+}
